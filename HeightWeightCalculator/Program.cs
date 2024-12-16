e7e33e2bb97d6169973bbf5acbffccdd22164132
Sofiane <132440690+Sofiane5900@users.noreply.github.com>
2024-12-16 14:03:28 +0100

    ajout des variables height & weight

--- HeightWeightCalculator/Program.cs
+++ HeightWeightCalculator/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace HeightWeightCalculator 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("--- Quel taille dois-je prendre ? ---");
+            Console.Write("Entrez votre taille (en cm) : ");
+            int personHeight = int.Parse(Console.ReadLine());
+            Console.Write("Entrez votre poids (en kg) : ");
+            int personWidth = int.Parse(Console.ReadLine());
+
+
+        }
+    }
+}
